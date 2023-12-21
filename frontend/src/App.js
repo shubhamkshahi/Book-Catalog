ddbb103fedee69b1280fc2227f972ba4eeb52f2a
shubhamkshahi <shubhamkshahi42@gmail.com>
2023-12-21 15:24:39 +0530

    adding header

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,20 @@
 import React from 'react'
-
+import { Container } from 'react-bootstrap'
+import Header from './components/Header'
+import Footer from './components/Footer'
 const App = () => {
   return (
-    <div>App</div>
+    <>
+    <Header/>
+    <main className='py-3'>
+      <Container />
+       
+      <h1>
+      welcome to the Book Catalog
+        </h1>
+        </main>
+        <Footer />
+    </>
   )
 }
 
---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,21 @@
+import {Navbar,Nav,Container} from 'react-bootstrap';
+import {FaShoppingCart, FaUser} from 'react-icons/fa';
+
+const Header = () => {
+  return (
+   <header>
+    <Navbar bg='dark' variant='dark' expand='lg' collapseOnSelect>
+     <Container>
+        <Navbar.Brand href='/'>Book Catalog</Navbar.Brand>
+        <Navbar.Toggle aria-controls='basic-navbar-nav' />
+        <Navbar.Collapse id = 'basic-navbar-nav'></Navbar.Collapse>
+        <Nav className='ms-auto'>
+        <Nav.Link href='/login'> <FaUser /> Sign In </Nav.Link>
+        </Nav>
+     </Container>
+    </Navbar>
+   </header>
+    )
+}
+
+export default Header
