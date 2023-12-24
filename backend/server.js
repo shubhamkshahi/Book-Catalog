2834762b0b9824a41f7aceb3241e4462c93482e9
shubhamkshahi <shubhamkshahi42@gmail.com>
2023-12-24 17:48:07 +0530

    add files

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,8 @@
 import express from 'express';
+import dotenv from 'dotenv';
 import products from './data/products.js';
-const port = 5000;
+dotenv.config();
+const port = process.env.PORT || 5000;
 const app = express();
 
 app.get('/',(req,res)=>{
